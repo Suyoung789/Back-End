3950be3cd9bd3078fe83f9fd515560bb691ff397
Suyoun789 <rsy011203@gmail.com>
2019-04-26 09:25:58 +0900

    rename Serializers to serializers

--- lifepet/app/urls.py
+++ lifepet/app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 from . import views
 
-urlpatterns= [
+urlpatterns = [
     path('login/', obtain_jwt_token),
     path('join/', views.UserView.as_view()),
     path('joinAdmin/', views.AdminUserView.as_view()),
--- lifepet/app/views.py
+++ lifepet/app/views.py
@@ -1,14 +1,14 @@
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets, views
 
-from . import Serializers
+from . import serializers
 
 
 class UserView(viewsets.generics.CreateAPIView):
-    serializer_class = Serializers.UserSerializer
+    serializer_class = serializers.UserSerializer
     model = get_user_model()
 
 
 class AdminUserView(viewsets.generics.CreateAPIView):
-    serializer_class = Serializers.AdminUserSerializer
+    serializer_class = serializers.AdminUserSerializer
     model = get_user_model()
